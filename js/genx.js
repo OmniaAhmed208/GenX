51635fdcf61bd80b5e0e91e463c4172f1d723fed
OmniaAhmed208 <oa66494@gmail.com>
2023-03-21 22:19:17 +0200

    complete pages

--- js/genx.js
+++ js/genx.js
@@ -1,4 +1,84 @@
-// ============= slider ==============
+// header
+let service = document.querySelector('#service');
+let serviceList = document.querySelector('#services');
+let about = document.querySelector('#about');
+let aboutList = document.querySelector('#aboutUs');
+let web_design = document.querySelector('#web-design');
+let web_designList = document.querySelector('#web-design-list');
+let android = document.querySelector('#android');
+let androidList = document.querySelector('#android-app');
+let signature = document.querySelector('#signature');
+let signatureList = document.querySelector('#signList');
+
+// =============== services
+service.onmouseover = function(){
+    serviceList.classList.add('show');
+}
+service.onmouseout = function(){
+    serviceList.classList.remove('show');
+}
+serviceList.onmouseover = function(){
+    serviceList.classList.add('show');
+}
+serviceList.onmouseout = function(){
+    serviceList.classList.remove('show');
+}
+//================ about 
+about.onmouseover = function(){
+    aboutList.classList.add('show');
+}
+about.onmouseout = function(){
+    aboutList.classList.remove('show');
+}
+aboutList.onmouseover = function(){
+    aboutList.classList.add('show');
+}
+aboutList.onmouseout = function(){
+    aboutList.classList.remove('show');
+}
+//================ web-design 
+web_design.onmouseover = function(){
+    web_designList.classList.add('show');
+}
+web_design.onmouseout = function(){
+    web_designList.classList.remove('show');
+}
+web_designList.onmouseover = function(){
+    web_designList.classList.add('show');
+}
+web_designList.onmouseout = function(){
+    web_designList.classList.remove('show');
+}
+//================ android 
+android.onmouseover = function(){
+    androidList.classList.add('show');
+}
+android.onmouseout = function(){
+    androidList.classList.remove('show');
+}
+androidList.onmouseover = function(){
+    androidList.classList.add('show');
+}
+androidList.onmouseout = function(){
+    androidList.classList.remove('show');
+}
+//================ signature 
+signature.onmouseover = function(){
+    signatureList.classList.add('show');
+}
+signature.onmouseout = function(){
+    signatureList.classList.remove('show');
+}
+signatureList.onmouseover = function(){
+    signatureList.classList.add('show');
+}
+signatureList.onmouseout = function(){
+    signatureList.classList.remove('show');
+}
+
+
+
+// ============================ slider ===============================
 $( function(){
     
     var windowHieight = $(window).height(),
@@ -7,7 +87,8 @@ $( function(){
     $('.slider,.carousel-item,.carousel-item img').height(windowHieight - navigationHeight);
 });
 
-// ============== slider-company-clients ============
+
+// ========================= slider-company-clients =======================
 $('.slid').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -16,6 +97,7 @@ $('.slid').slick({
     prevArrow: '<span class="prev-arrow"><i class="fas fa-angle-left"></i></span>',
     nextArrow: '<span class="next-arrow"><i class="fas fa-angle-right"></i></span>',
 });
+
 
 // ============== loading ==============
 $(window).on('load', function () {
